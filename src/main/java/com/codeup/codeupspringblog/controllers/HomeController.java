e87dae538176be56d9b27763e740b838c6127fdf
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-03-30 09:18:45 -0500

    this commit fixed the footer and home page of the project along with some other styling changes

--- src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
@@ -1,15 +1,27 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.models.Post;
+import com.codeup.codeupspringblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 class HomeController {
 
-    @GetMapping("/")
+    private final PostRepository postDao;
 
-    public String landingPage(){
+    public HomeController(PostRepository postDao) {
+        this.postDao = postDao;
+    }
+
+    @GetMapping("/")
+    public String landingPage(Model model){
+        List<Post> posts = postDao.findAll();
+        model.addAttribute("posts", posts);
         return "home";
     }
 }
